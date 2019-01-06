4748efcad9f682d0c4adc51733b70b6c0653ddd5
emard <vordah@gmail.com>
2019-01-07 00:26:37 +0100

    animated demo

--- emard/top_cable.v
+++ emard/top_cable.v
@@ -9,6 +9,9 @@ module top
     // prevent ESP32 firmware from taking control of the board
     // (we don't want it write its "passthru" bitstream)
     assign wifi_gpio0 = 1'b1;
+    
+    wire pixclk;
+    assign pixclk = clk_25mhz;
 
     wire [6:0] ADDRX;
     wire [4:0] ADDRY;
@@ -16,7 +19,7 @@ module top
     ledscan
     ledscan_inst
     (
-        .clk(clk_25mhz),
+        .clk(pixclk),
         .addrx(ADDRX),
         .addry(ADDRY),
         .latch(LATCH),
@@ -24,29 +27,57 @@ module top
     );
     
     reg [22:0] blinky;
-    always @(posedge clk_25mhz)
+    always @(posedge pixclk)
     begin
       blinky <= blinky + 1;
     end
     assign led[7] = blinky[22];
 
+
     // simple animation counter
     reg [26:0] ANIM;
-    always @(posedge clk_25mhz)
+    always @(posedge pixclk)
     begin
       ANIM <= ANIM + 1;
     end
+
+    // test picture generator
+    wire [7:0] CounterX, CounterY;
+    wire [4:0] ADDRY0;
+    assign ADDRY0 = ADDRY+1;
+    assign CounterX = ADDRX + ANIM[26:20];
+    assign CounterY = {1'b0,ADDRY0} + ANIM[26:18];
+    wire [7:0] W = {8{CounterX[7:0]==CounterY[7:0]}};
+    wire [7:0] A = {8{CounterX[7:5]==3'h2 && CounterY[7:5]==3'h2}};
+    wire [7:0] red, green, blue;
+    assign red = ({CounterX[5:0] & {6{CounterY[4:3]==~CounterX[4:3]}}, 2'b00} | W) & ~A;
+    assign green = (CounterX[7:0] & {8{CounterY[6]}} | W) & ~A;
+    assign blue = CounterY[7:0] | W | A;
+
+    // same as above but for lower half of the display (32 pixels below)
+    wire [7:0] CounterY1;
+    assign CounterY1 = {1'b1,ADDRY0} + ANIM[26:18];
+    wire [7:0] W1 = {8{CounterX[7:0]==CounterY1[7:0]}};
+    wire [7:0] A1 = {8{CounterX[7:5]==3'h2 && CounterY1[7:5]==3'h2}};
+    wire [7:0] red1, green1, blue1;
+    assign red1 = ({CounterX[5:0] & {6{CounterY1[4:3]==~CounterX[4:3]}}, 2'b00} | W1) & ~A1;
+    assign green1 = (CounterX[7:0] & {8{CounterY1[6]}} | W1) & ~A1;
+    assign blue1 = CounterY[7:0] | W1 | A1;
+
     wire [2:0] RGB0, RGB1;
+    assign RGB0 = {blue[7], green[7], red[7]};
+    assign RGB1 = {blue1[7], green1[7], red1[7]};
     // combinatorial logic which creates the picture
     // assign RGB0 = ADDRX[2:0] == 7 ? 3'b010 : 3'b100; // 3'bBGR
-    assign RGB0 = ADDRX[2:0] == ADDRY[2:0] + ANIM[26:24] ? 3'b010 : 3'b100; // 3'bBGR
-    assign RGB1 = ADDRX[2:0] == ANIM[26:24] ? 3'b011 : 3'b100; // 3'bBGR
+    // assign RGB0 = ADDRX[2:0] == ADDRY[2:0] + ANIM[26:24] ? 3'b010 : 3'b100; // 3'bBGR
+    // assign RGB1 = ADDRX[2:0] == ANIM[26:24] ? 3'b011 : 3'b100; // 3'bBGR
+
 
     // output pins mapping to ULX3S board    
     wire [27:0] ogp, ogn;
     assign ogp[14] = ADDRY[4];
     assign ogn[14] = ADDRY[3];
-    assign ogp[15] = clk_25mhz; // SCLK display clock
+    assign ogp[15] = pixclk; // SCLK display clock
     assign ogn[15] = ADDRY[2];
     assign ogp[16] = LATCH;
     assign ogn[16] = ADDRY[1];
