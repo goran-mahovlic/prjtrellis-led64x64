a9aeec4f5e0e5e2dd2fe4604a71bf7de1147b4af
Igor Brkic <igor@hyperglitch.com>
2019-01-11 22:36:20 +0100

    gamma correction, path for palette and debug image

--- converter/cnv.py
+++ converter/cnv.py
@@ -16,14 +16,17 @@ def main():
     parser.add_argument('-s', '--size', help="Output size WxH (default 64x64, if set to 'same' will take input image's size)", default='64x64', type=str)
     parser.add_argument('-a', '--keep-aspect-ratio', help="Kepp aspect ratio when resizing", action='store_true')
     parser.add_argument('-d', '--dither', help="Enable dithering (default false)", action='store_true')
-    parser.add_argument('-b', '--debug', help="save output as /tmp/out.png image", action='store_true')
+    parser.add_argument('-b', '--debug', help="save output to provided image file", type=str)
     parser.add_argument('-c', '--colors', help="Number of colors in palette (default 16)", default=16, type=int)
+    parser.add_argument('-g', '--gamma', help="gamma correction", default=1.0, type=float)
     parser.add_argument('-o', '--output', help="Output file name (default output.mem)", default="output.mem")
+    parser.add_argument('-p', '--palette', help="Save palette to file instead of printing to stdout", type=str)
     parser.add_argument('input_image', help="Input image (jpg, png, gif)")
     args = parser.parse_args()
 
     try:
         im = Image.open(args.input_image)
+        im = im.convert('RGB')
     except IOError:
         print("Invalid input image")
         return
@@ -47,23 +50,34 @@ def main():
         else:
             im = im.resize(size, resample=Image.LANCZOS)
 
+    # gamma correction
+    if args.gamma!=1.0:
+        for y in range(im.size[0]):
+            for x in range(im.size[1]):
+                px = tuple([int(((c/255.0)**args.gamma)*255) for c in im.getpixel((x, y))])
+                im.putpixel((x,y), px)
+
     if args.dither:
         im = im.convert('RGB', colors=args.colors)
     im = im.quantize(args.colors)
     im = im.convert('RGB')
 
     if args.debug:
-        im.save('/tmp/out.png')
+        im.save(args.debug)
 
     palette = [c[1] for c in im.getcolors()]
     if len(palette)<args.colors:
         palette += [(0,0,0)]*(args.colors-len(palette))
     
     # print out palette
-    with open("palette.raw", 'w') as p:
+    if args.palette:
+        with open(args.palette, 'w') as p:
+            for idx, c in enumerate(palette):
+                p.write("%02x%02x%02x\n"%(c[0], c[1], c[2]))
+    else:
         for idx, c in enumerate(palette):
             print("assign palette[%d] = 24'h%02x%02x%02x;" % (idx, c[0], c[1], c[2]))
-            p.write("%02x%02x%02x\n"%(c[0], c[1], c[2]))
+
     # save output image
     with open(args.output, 'w') as f:
         for y in range(im.height):
